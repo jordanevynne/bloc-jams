5368ab78f43243ce682960275a1c3c6353fe3064
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-08-28 00:04:03 -0700

    checkpoint 33 assignment: completed

--- scripts/album.js
+++ scripts/album.js
@@ -56,10 +56,10 @@ var clickHandler = function() {
         $(this).html(pauseButtonTemplate);
         $('.main-controls .play-pause').html(playerBarPauseButton);
         currentSoundFile.play();
-      } else {
-          $(this).html(playButtonTemplate);
-          $('.main-controls .play-pause').html(playerBarPlayButton);
-          currentSoundFile.pause();
+    } else {
+        $(this).html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
       }
     }
 };
@@ -86,7 +86,7 @@ var offHover = function(event) {
   $row.hover(onHover, offHover);
   return $row;
   
-  console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
+//  console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
 };
 
 
@@ -137,12 +137,13 @@ var nextSong = function() {
   if (currentSongIndex >= currentAlbum.songs.length) {
       currentSongIndex = 0;
   }
-
+  
   setSong(currentSongIndex + 1);
   currentSoundFile.play();
   updatePlayerBarSong();
   // currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
+  // player bar information
   $('.currently-playing .song-name').text(currentSongFromAlbum.title);
   $('.currently-playing .artist-name').text(currentAlbum.artist);
   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
@@ -187,6 +188,18 @@ var previousSong = function() {
   $lastSongNumberCell.html(lastSongNumber);   
 };
 
+var togglePlayFromPlayerBar = function() {
+  var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+  if (currentSoundFile.isPaused()) {
+    $currentlyPlayingCell.html(pauseButtonTemplate);
+    $(this).html(playerBarPauseButton);
+    currentSoundFile.play();
+  } else if (currentSoundFile) {
+    $currentlyPlayingCell.html(playButtonTemplate);
+    $(this).html(playerBarPlayButton);
+    currentSoundFile.pause();
+  }
+};
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -203,9 +216,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
+  $playPauseButton.click(togglePlayFromPlayerBar);
 });
